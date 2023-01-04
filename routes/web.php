64ea5ffdcd963034e472a5f8cbd7ac5e853bdf13
Achmad Fahresya Rayhannur <af.rayhannur@gmail.com>
2023-01-04 14:25:14 +0700

    some changes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/product', function () {
+    return view('product');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
